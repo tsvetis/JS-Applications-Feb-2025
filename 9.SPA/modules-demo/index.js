3808fb6f83f0dde0df624a7a893667174a99423e
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2025-02-27 22:10:21 +0200

    SPA

---
+++ 9.SPA/modules-demo/index.js
@@ -0,0 +1,11 @@
+import { sum, subtract } from "./calculator.js";
+import calc from "./calculator.js";
+// import * as test123 from "./calculator.js";
+
+console.log(sum(5, 12));
+console.log(subtract(55, 12));
+console.log(calc.multiply(5, 6));
+console.log(calc.divide(60, 6));
+
+// console.log(test123.default.sum(5, 5));
+// console.log(test123.subtract(10, 5));
